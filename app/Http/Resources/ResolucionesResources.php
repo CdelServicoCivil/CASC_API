c31b15432b51e711bb65513e0a17d6d134fbce8b
Berserker156 <luissequeiraflores@gmail.com>
2024-04-23 16:14:34 -0600

    Main

--- app/Http/Resources/ResolucionesResources.php
+++ app/Http/Resources/ResolucionesResources.php
@@ -5,7 +5,7 @@ namespace App\Http\Resources;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
 
-class Resoluciones extends JsonResource
+class ResolucionesResources extends JsonResource
 {
     /**
      * Transform the resource into an array.
